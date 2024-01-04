f9aba374c442f24d513e20564a82d3acb6b9b0c1
Yury <sinkevich.work@gmail.com>
2024-01-04 22:15:35 +0300

    Done v1.1

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,8 @@
 package com.epam.utils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return NumberUtils.toInt(str,0) < 0;
     }
 }
